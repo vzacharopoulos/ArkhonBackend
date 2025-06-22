6a9ba3a0b2efdb36d655954f776f090ff015d6d3
vzacharopoulos <vzacharopoulos#@gmail.com>
2025-06-22 12:07:43 +0300

    pporderlines got ttm

--- src/pporderlines2/pporderlines2.service.ts
+++ src/pporderlines2/pporderlines2.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PPackages } from 'src/entities/entities/PPackages.entity';
 import { Pporderlines2 } from 'src/entities/entities/Pporderlines2.entity';
+import { ProdOrdersView } from 'src/entities/views/PanelProductionOrdersview-with-iscanceled';
 import { PanelSpeeds } from 'src/entities/views/PanelSpeeds';
 import { Repository } from 'typeorm';
 
@@ -15,16 +16,34 @@ export class Pporderlines2Service {
     private readonly ppackagesRepository: Repository<PPackages>,
     @InjectRepository(PanelSpeeds)
     private readonly panelSpeedsRepository: Repository<PanelSpeeds>,
+    @InjectRepository(ProdOrdersView)
+    private readonly prodOrdersRepository: Repository<ProdOrdersView>,
   ) {}
 
   async findAll(): Promise<Pporderlines2[]> {
-    return  this.pporderlines2Repository.find();
-    
+    return this.pporderlines2Repository
+      .createQueryBuilder('line')
+      .leftJoinAndMapOne(
+        'line.prodOrdersView',
+        ProdOrdersView,
+        'prodOrdersView',
+        'line.custporderno COLLATE SQL_Latin1_General_CP1_CI_AS = prodOrdersView.prodOrder'
+      )
+      .getMany();
+
   }
 
   async findOne(id: number): Promise<Pporderlines2 | null> {
-    return this.pporderlines2Repository.findOne({ where: { id } });
-    
-  }
+    return this.pporderlines2Repository
+      .createQueryBuilder('line')
+      .leftJoinAndMapOne(
+        'line.prodOrdersView',
+        ProdOrdersView,
+        'prodOrdersView',
+        'line.custporderno COLLATE SQL_Latin1_General_CP1_CI_AS = prodOrdersView.prodOrder'
+      )
+      .where('line.id = :id', { id })
+      .getOne();
 
+  }
 }
